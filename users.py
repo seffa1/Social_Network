2449a69f918434b483ac4dc4e58b0c4737429630
se_laptop <sameffa1@gmail.com>
2021-11-27 11:24:37 -0500

    Deleted user file

--- users.py
+++ users.py
@@ -1,15 +1,2 @@
-from collections import deque
-
-
-name_list = deque()
-email_list = deque()
-
-names_to_read = 1000
-n = 0
-while n <= names_to_read:
-    with open('names.txt', 'r') as name_file:
-        for name in name_file.readlines():
-            name_list.append(name)
-            n += 1
 
 
