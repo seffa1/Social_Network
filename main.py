72dc54041d76891fd60285957badcd1f6f75d06b
seffa_work <sameffa1@gmail.com>
2021-11-29 17:00:17 -0500

    added connectedness global

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ import time
 # GLOBALS
 NETWORK_SIZE = 300
 
+# 1 is very connected, 100 is not so connected
+# Exp: 10 means a user can have a most 1/10th of the total network as friends
+CONNECTEDNESS = 1
+
 
 def clear():
     os.system('cls')
@@ -15,7 +19,7 @@ def main():
     Network_Generator.generate_user_info(NETWORK_SIZE)
     Network_Generator.generate_users()
     Network_Generator.show_user()
-    Network_Generator.generate_user_friends()
+    Network_Generator.generate_user_friends(CONNECTEDNESS)
     Network_Generator.print_connections()
 
 
