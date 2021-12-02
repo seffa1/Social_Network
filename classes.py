e5cc05ab5c0ffb1ef963f08907fe28ed8dfaced3
se_laptop <sameffa1@gmail.com>
2021-12-02 05:44:23 -0500

    "added a terminals, working on user search"

--- classes.py
+++ classes.py
@@ -1,8 +1,8 @@
 import os
 import random
 from collections import deque
-import networkx as nx
-import matplotlib.pyplot as plt
+# import networkx as nx
+# import matplotlib.pyplot as plt
 
 def clear():
     os.system('cls')
@@ -16,6 +16,8 @@ class User:
         self.age = age
         self.name = name
         self.gender = gender
+
+        # This should be a sorted dictionary, by weight
         self.friends = {}  # email: weight
 
         User.user_ID += 1
@@ -55,8 +57,7 @@ class Network_Generator:
 
     @classmethod
     def generate_user_info(cls, user_amount):
-        print(f''
-              f'Generating info for {user_amount} users...')
+        print(f'Generating info for {user_amount} users...')
         n = 0
 
         # Generates the names, age, and gender for the users from a text file of names
@@ -92,7 +93,6 @@ class Network_Generator:
     def generate_users(cls):
         # Its unlikely but technically possible to have two users with the same email generated
         # In that case the second one would overwrite the first
-
         print('\nGenerating user accounts with user info....')
         while len(cls.name_list) > 0:
             name = cls.name_list.pop()
@@ -101,7 +101,6 @@ class Network_Generator:
             gender = cls.gender_list.pop()
             user = User(name, email, age, gender)
             cls.add_user(user)
-
         print('Done')
 
     @classmethod
@@ -111,20 +110,7 @@ class Network_Generator:
             print(f'ID: {cls.user_dict[user].user_ID}   Email: {user}')
         print('----------------------\n')
 
-    # This method needs re-written
-    # It should be choosing two friends at random, if they dont have a connection, make a connection with a weight
-    # Then add each other to each other's friends list
-    # This way there is one edge with one weight instead of two
-    # @classmethod
-    # def generate_user_friends(cls, connectedness: int):
-    #     print('Generating friend network...')
-    #     for user_email in cls.user_dict:
-    #         user = cls.user_dict[user_email]
-    #         number_of_friends = random.randint(1, len(cls.user_dict) // connectedness)  # at most you can be friends with a 1/3 of total users
-    #         for i in range(0, number_of_friends):
-    #             friend = random.choice(list(cls.user_dict.values()))
-    #             likeness = random.randint(0, 9)
-    #             user.add_friend(friend, likeness)
+    connections = 0
 
     @classmethod
     def make_friends(cls, user1: User, user2: User, weight):
@@ -134,39 +120,105 @@ class Network_Generator:
             # friends = {email: weight}
             user1.friends[user2.email] = weight
             user2.friends[user1.email] = weight
-            print(f'{user1.email} and {user2.email} are now friends: {weight}')
+            # print(f'{user1.email} and {user2.email} are now friends: {weight}')
+            cls.connections += 1
 
     @classmethod
-    def generate_connections(cls, connectedness: int):
-        print('Generating connections...')
+    def generate_connections(cls, max_friends: int):
+        print('\nGenerating connections...')
+
+        # each user will be assigned some friends
         for user in cls.user_dict.values():
-            pass
+
+            # each user gets a random number of friend between 1 and total_users/connectedness
+            number_of_friends = random.randint(1, max_friends)
+
+            # add a number of friends equal to the number_of_friends
+            for i in range(0, number_of_friends):
+
+                # Choose a random user from the list, but try again if you pick yourself
+                friend = random.choice(list(cls.user_dict.values()))
+                while friend.email == user.email:
+                    friend = random.choice(list(cls.user_dict.values()))
+
+                # Give them a random likeness, or how much they interact with one another
+                likeness = random.randint(0, 9)
+
+                # Make them friends!
+                cls.make_friends(user, friend, likeness)
+        print(f'Total connections: {cls.connections}')
+        print(f'Average Friends: {cls.connections // len(cls.user_dict)}')
+        print('Done')
+
 
     @classmethod
     def print_connections(cls):
-        input("\nPress enter to print connections...")
         for user in cls.user_dict.values():
             print(f'Friends of User ID:{user.user_ID} - {user.email}')
             for friend in user.friends:
                 print(f'---> Weight: {user.friends[friend]} - ID: {cls.user_dict[friend].user_ID} - {friend}')
             print('')
-        print('----------------------\n')
+
+        print('----------------------')
 
     @classmethod
-    def count_connections(cls):
-        print("Counting duplicate connections...")
-        duplicate_count = 0
-        connection_count = 0
-        for user in cls.user_dict.values():
-            for friend_email in user.friends:
-                connection_count += 1
-                friend = cls.user_dict[friend_email]
-                if user.email in friend.friends:
-                    duplicate_count += 1
-                    print(f"Duplicate: {user.user_ID} and {friend.user_ID}")
-        print(f'Total Connections: {connection_count}')
-        print(f'Duplicate Connections: {duplicate_count}')
-        print('----------------------\n')
+    def create_user(cls, name, email, age, gender):
+        user = User(name, email, age, gender)
+        cls.add_user(user)
+
+    @classmethod
+    def search_for_user(cls, name):
+        pass
+
+    @classmethod
+    def find_user_bredth(cls, user1: User, user2: User):
+        # This algorithm starts at user 1, and traverses the network until it finds user2
+        visit_count = 0
+        visited = []
+        path_queue = deque()
+        current_user = user1
+
+        # The search ends if this condition is met
+        if current_user.email == user2.email:
+            print('User Found!')
+            print(f'Visited {visit_count} user')
+            print(f'Visited the following users:')
+            for user in visited:
+                print(f'{user.name}')
+            return
+
+        # If the user is not the one we are looking for
+
+        # Get all friends of the current user
+        current_user_friends_list = []
+        for friend_email in current_user.friends:
+            friend = cls.user_dict[friend_email]
+            current_user_friends_list.append(friend)
+
+        # We add their connections to the path_queue, then chose one at random
+        for friend in current_user_friends_list:
+            path_queue.appendleft(friend)
+
+
+
+        # Make the chosen children the current child, check it, add its children to the queue
+        # The next child is the next in the queue, this is a bredth-first approach
+        # For a depth first search, we will use a stack instead of a queue
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # https://www.geeksforgeeks.org/visualize-graphs-in-python/
--- main.py
+++ main.py
@@ -3,29 +3,102 @@ from classes import *
 import time
 
 
-# GLOBALS
-NETWORK_SIZE = 10
-
-# 1 is very connected, 100 is not so connected
-# Exp: 10 means a user can have a most 1/10th of the total network as friends
-CONNECTEDNESS = 1
-
-
 def clear():
     os.system('cls')
 
 
-def main_menu():
+# GLOBALS
+NETWORK_SIZE = 0
+MAX_FRIENDS = 0
+
+
+# This will be the menu that you interact with
+class Terminal:
     pass
 
 
-def main():
+def initialize():
+    clear()
+    print("Welcome to bookface, configure network...")
+    print("Select network size:")
+    print('1 ---> small')
+    print('2 ---> medium')
+    print('3 ---> large')
+
+    options = ['1', '2', '3']
+
+    a = ''
+
+    while a not in options:
+        a = input("--->")
+
+    if a == '1':
+        NETWORK_SIZE = 10
+    elif a == '2':
+        NETWORK_SIZE = 100
+    else:
+        NETWORK_SIZE = 1000
+
+    print(f'\nSelect max friend count:')
+    print('1 ---> 10% of total users')
+    print('2 ---> 50% of total users')
+    print('3 ---> 100% of total users')
+
+    a = ''
+
+    while a not in options:
+        a = input("--->")
+
+    if a == '1':
+        MAX_FRIENDS = NETWORK_SIZE // 10
+    elif a == '2':
+        MAX_FRIENDS = NETWORK_SIZE // 2
+    else:
+        MAX_FRIENDS = NETWORK_SIZE
+
+    print('\n-------------------------')
+    print(f'PARAMETERS SELECTED')
+    print(f'Network Size: {NETWORK_SIZE}')
+    print(f'Max Friend Count: {MAX_FRIENDS}')
+    print('\n-------------------------')
+    input("Press enter to generate the network")
+    clear()
     Network_Generator.generate_user_info(NETWORK_SIZE)
     Network_Generator.generate_users()
-    Network_Generator.show_user()
-    Network_Generator.generate_user_friends(CONNECTEDNESS)
-    Network_Generator.print_connections()
-    Network_Generator.count_connections()
+    Network_Generator.generate_connections(MAX_FRIENDS)
+    input('\nPress Enter to Continue')
+
+def terminal():
+    while True:
+        clear()
+        options = ['1', '2', '3', 'X', 'x']
+
+        print('Select Action:')
+        print('1 ---> Show Users')
+        print('2 ---> Print Connections')
+        print('3 ---> Find Path Between Users')
+        print('X ---> Exit\n')
+
+        a = ''
+        while a not in options:
+            a = input('--->')
+
+        if a == '1':
+            Network_Generator.show_user()
+            input('Press enter to continue')
+        elif a == '2':
+            Network_Generator.print_connections()
+            input('Press enter to continue')
+        elif a == '3':
+            pass
+            input('Press enter to continue')
+        else:
+            break
+
+
+def main():
+    initialize()
+    terminal()
 
 
 if __name__ == '__main__':
